c56e351a962fbadc94dc3b0d9ff2d57711bc0a6c
Daniel Furman <djfurman@gmail.com>
2017-11-04 07:45:31 -0400

    More codacy fixes to the dusk data.

--- tests/Unit/PartiesTest.php
+++ tests/Unit/PartiesTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Party;
+use App\Agreement;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -13,8 +14,20 @@ class PartiesTest extends TestCase
     /** @test */
     public function it_tracks_the_party()
     {
-        $party = factory(Party::class)->create();
+        factory(Party::class)->create();
         $this->assertCount(1, Party::all());
     }
 
+    /** @test */
+    public function it_has_many_agreements()
+    {
+        $party = factory(Party::class)->create();
+
+        $agreements = factory(Agreement::class, 3)->create();
+        $agreements->each(function ($agreement) use ($party) {
+            $agreement->addSeller($party);
+        });
+
+        $this->assertCount(3, $party->agreements()->get());
+    }
 }
