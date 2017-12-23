91d75660ac805f1c7c99bdf37dfbff3232051f88
Daniel Furman <djfurman@gmail.com>
2017-12-23 17:33:17 -0500

    Add treasury data fetch functionality.

--- app/SloaAccountingLine.php
+++ app/SloaAccountingLine.php
@@ -6,7 +6,29 @@ class SloaAccountingLine extends AccountingLine
 {
     public function treasuryData()
     {
-        return null;
+        $issueYear = 'XXXX';
+        $closeYear = 'XXXX';
+
+        if (is_null($this->availability_type)) {
+            if (!isset($this->bpoa)) {
+                throw new Exception("BPOA is not provided");
+            }
+
+            if (!isset($this->epoa)) {
+                throw new Exception("EPOA is not provided");
+            }
+
+            $issueYear = $this->bpoa->year;
+            $closeYear = $this->epoa->year;
+        }
+
+        return [
+            'department_regular' => $this->department_regular,
+            'department_transfer' => $this->department_transfer,
+            'fiscal_year' => $issueYear.$closeYear,
+            'main_account' => $this->main_account,
+            'sub_allocation' => $this->sub_allocation
+        ];
     }
 
     public function expiresInFiscalYear()
