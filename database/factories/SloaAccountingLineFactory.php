9fc655c4cc9b0c36cb15f00eb3b87a5c53d1da5b
Daniel Furman <djfurman@gmail.com>
2017-12-23 13:17:10 -0500

    Add SloaAccountingLine model implementation for first 14 SLOA elements.

--- database/factories/SloaAccountingLineFactory.php
+++ database/factories/SloaAccountingLineFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use App\SloaAccountingLine;
 use Faker\Generator as Faker;
 
@@ -7,6 +8,18 @@ $factory->define(SloaAccountingLine::class, function (Faker $faker) {
     return [
         'sub_class' => null,
         'department_transfer' => null,
-        'department_regular' => $faker->randomElement(['021', '017', '057', '097'])
+        'department_regular' => $faker->randomElement(['021', '017', '057', '097']),
+        'bpoa' => Carbon::instance($faker->dateTimeInInterval('now', '+ 1 year')),
+        'epoa' => Carbon::instance($faker->dateTimeBetween('+ 2 years', '+ 3 years')),
+        'availability_type' => null,
+        'main_account' => $faker->regexify('[A-Z0-9]{4}'),
+        'sub_account' => $faker->regexify('[0-9]{3}'),
+        'business_event_type_code' => $faker->regexify('[A-Z0-9]{1,8}'),
+        'object_class' => $faker->regexify('[0-9]{3,6}'),
+        'reimbursable_flag' => $faker->randomElement(['D', 'R']),
+        'budget_line_item' => $faker->regexify('[A-Z0-9]{3,16}'),
+        'security_cooperation' => $faker->countryISOAlpha3,
+        'security_cooperation_implementing_agency_code' => $faker->randomElement(['B', 'D', 'P']),
+        'security_cooperation_case_designator' => $faker->regexify('[A-Z0-9]{3,4}'),
     ];
 });
--- tests/Unit/SloaAccountingLineTest.php
+++ tests/Unit/SloaAccountingLineTest.php
@@ -72,4 +72,214 @@ class SloaAccountingLineTest extends TestCase
         $this->assertNotNull($sloaAccountingLine->department_regular);
         $this->assertDatabaseHas('sloa_accounting_lines', ['department_regular' => $sloaAccountingLine->department_regular]);
     }
+
+    // TODO figure out how to assert that if you do pass null as department_regular an exception is thrown as an expected test
+
+    /** @test */
+    public function it_tracks_the_beginning_period_of_availability_fiscal_year_date()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->bpoa);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['bpoa' => $sloaAccountingLine->bpoa]);
+    }
+
+    /** @test */
+    public function it_allows_the_beginning_period_of_availability_fiscal_year_date_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['bpoa' => null]);
+
+        $this->assertNull($sloaAccountingLine->bpoa);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['bpoa' => $sloaAccountingLine->bpoa]);
+    }
+
+    /** @test */
+    public function it_tracks_the_ending_period_of_availability_fiscal_year_date()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->epoa);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['epoa' => $sloaAccountingLine->epoa]);
+    }
+
+    /** @test */
+    public function it_allows_the_ending_period_of_availability_fiscal_year_date_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['epoa' => null]);
+
+        $this->assertNull($sloaAccountingLine->epoa);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['epoa' => $sloaAccountingLine->epoa]);
+    }
+
+    /** @test */
+    public function it_tracks_the_availability_type()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['availability_type' => 'X']);
+
+        $this->assertNotNull($sloaAccountingLine->availability_type);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['availability_type' => $sloaAccountingLine->availability_type]);
+    }
+
+    /** @test */
+    public function it_allows_the_availability_type_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['availability_type' => null]);
+
+        $this->assertNull($sloaAccountingLine->availability_type);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['availability_type' => $sloaAccountingLine->availability_type]);
+    }
+
+    /** @test */
+    public function it_tracks_the_main_account()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->main_account);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['main_account' => $sloaAccountingLine->main_account]);
+    }
+
+    /** @test */
+    public function it_tracks_the_sub_account()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->sub_account);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['sub_account' => $sloaAccountingLine->sub_account]);
+    }
+
+    /** @test */
+    public function it_allows_the_sub_account_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['sub_account' => null]);
+
+        $this->assertNull($sloaAccountingLine->sub_account);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['sub_account' => $sloaAccountingLine->sub_account]);
+    }
+
+    /** @test */
+    public function it_tracks_the_business_event_type_code()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->business_event_type_code);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['business_event_type_code' => $sloaAccountingLine->business_event_type_code]);
+    }
+
+    /** @test */
+    public function it_allows_the_business_event_type_code_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['business_event_type_code' => null]);
+
+        $this->assertNull($sloaAccountingLine->business_event_type_code);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['business_event_type_code' => $sloaAccountingLine->business_event_type_code]);
+    }
+
+    /** @test */
+    public function it_tracks_the_object_class()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->object_class);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['object_class' => $sloaAccountingLine->object_class]);
+    }
+
+    /** @test */
+    public function it_allows_the_object_class_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['object_class' => null]);
+
+        $this->assertNull($sloaAccountingLine->object_class);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['object_class' => $sloaAccountingLine->object_class]);
+    }
+
+    /** @test */
+    public function it_tracks_the_reimbursable_flag()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->reimbursable_flag);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['reimbursable_flag' => $sloaAccountingLine->reimbursable_flag]);
+    }
+
+    /** @test */
+    public function it_allows_the_reimbursable_flag_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['reimbursable_flag' => null]);
+
+        $this->assertNull($sloaAccountingLine->reimbursable_flag);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['reimbursable_flag' => $sloaAccountingLine->reimbursable_flag]);
+    }
+
+    /** @test */
+    public function it_tracks_the_budget_line_item()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->budget_line_item);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['budget_line_item' => $sloaAccountingLine->budget_line_item]);
+    }
+
+    /** @test */
+    public function it_allows_the_budget_line_item_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['budget_line_item' => null]);
+
+        $this->assertNull($sloaAccountingLine->budget_line_item);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['budget_line_item' => $sloaAccountingLine->budget_line_item]);
+    }
+
+    /** @test */
+    public function it_tracks_the_security_cooperation()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->security_cooperation);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['security_cooperation' => $sloaAccountingLine->security_cooperation]);
+    }
+
+    /** @test */
+    public function it_allows_the_security_cooperation_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['security_cooperation' => null]);
+
+        $this->assertNull($sloaAccountingLine->security_cooperation);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['security_cooperation' => $sloaAccountingLine->security_cooperation]);
+    }
+
+    /** @test */
+    public function it_tracks_the_security_cooperation_implementing_agency_code()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->security_cooperation_implementing_agency_code);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['security_cooperation_implementing_agency_code' => $sloaAccountingLine->security_cooperation_implementing_agency_code]);
+    }
+
+    /** @test */
+    public function it_allows_the_security_cooperation_implementing_agency_code_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['security_cooperation_implementing_agency_code'=> null]);
+
+        $this->assertNull($sloaAccountingLine->security_cooperation_implementing_agency_code);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['security_cooperation_implementing_agency_code' => $sloaAccountingLine->security_cooperation_implementing_agency_code]);
+    }
+
+    /** @test */
+    public function it_tracks_the_security_cooperation_case_designator()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create();
+
+        $this->assertNotNull($sloaAccountingLine->security_cooperation_case_designator);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['security_cooperation_case_designator' => $sloaAccountingLine->security_cooperation_case_designator]);
+    }
+
+    /** @test */
+    public function it_allows_the_security_cooperation_case_designator_to_be_null()
+    {
+        $sloaAccountingLine = factory(SloaAccountingLine::class)->create(['security_cooperation_case_designator' => null]);
+
+        $this->assertNull($sloaAccountingLine->security_cooperation_case_designator);
+        $this->assertDatabaseHas('sloa_accounting_lines', ['security_cooperation_case_designator' => $sloaAccountingLine->security_cooperation_case_designator]);
+    }
+    // TODO write a method that makes this thing a hat delimited string
 }
