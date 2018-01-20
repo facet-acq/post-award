1925583df029a9dc2958e3bfc8cff24faf66096f
Daniel Furman <djfurman@gmail.com>
2018-01-20 18:13:28 -0500

    Refactor Uuid trait properties into abstract class to simlify codebase.

--- app/UuidModel.php
+++ app/UuidModel.php
@@ -2,24 +2,16 @@
 
 namespace App;
 
-use \App\Traits\IdentifyByUuid;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class UuidModel extends Model
 {
-    use IdentifyByUuid;
-
     /**
     * Inform the eloquent model that we are using UUID-4s not incrementing counts
     *
     * @var boolean
     */
     public $incrementing = false;
-
-    public function getIncrementing()
-    {
-        return false;
-    }
 
     /**
      * Sets the name of the primary key for Eloquent
@@ -34,4 +26,47 @@ abstract class UuidModel extends Model
      * @var string
      */
     protected $keyType = 'uuid';
+
+    /**
+    * Setup model event hooks
+    *
+    * @return void
+    */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->uuid = (string) \Uuid::generate(4);
+        });
+    }
+
+    /**
+    * Setup route model binding for UUID
+    *
+    * @return string
+    */
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
+
+    /**
+    * Getter to integrate uuid value into Eloquent functionality
+    *
+    * @return string
+    */
+    public function getKeyName()
+    {
+        return 'uuid';
+    }
+
+    /**
+     * Getter to disable automatic incrementing values in Eloquent models
+     *
+     * @return boolean
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
 }
