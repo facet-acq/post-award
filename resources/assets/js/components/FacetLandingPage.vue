b2018638de71e450488322d8cdea1eb78ab7d694
paulmakarov <pmakarov@kfgisit.com>
2018-04-12 21:17:32 -0400

    added back in .env.example, modified .gitignore to ignore vscode directory, removed commented out html in vue pages

--- resources/assets/js/components/FacetLandingPage.vue
+++ resources/assets/js/components/FacetLandingPage.vue
@@ -19,20 +19,7 @@
         This system is built on powerful open source frameworks leveraging each for its core strengths and handing off defined contacts between system segments.
     </p>
   </div>
-    <!-- <div class="container is-fluid">
-      <h1>FACET</h1>
-
-      <section>
-        <h1>Getting Started Guide</h1>
-        <h2>FACET Post Award</h2>
-
-        <p>Post Award is contract entitlement and administration system designed arond the federated microservices computing model and built from the ground up for cloud-based computuing and microdeployments as needed.</p>
-
-        <h2>Technologies</h2>
-
-        <p>This system is built on powerful open source frameworks leveraging each for its core strengths and handing off defined contacts between system segments.</p>
-      </section>
-    </div> -->
+   
   <section class="container">
   <div class="columns features">
     <div class="column is-4">
@@ -84,9 +71,9 @@
 </template>
 
 <script>
-  export default {
-      mounted() {
-          console.log('Root component mounted.')
-      }
+export default {
+  mounted() {
+    console.log("Root component mounted.");
   }
+};
 </script>
